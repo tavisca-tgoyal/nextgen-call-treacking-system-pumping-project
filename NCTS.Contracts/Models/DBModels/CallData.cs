e0db79d67b5f599109e82f81bebcf236b24d64ed
Tushar Goyal <tgoyal@tavisca.in>
2019-10-21 15:41:55 +0530

    completed alarm name fucntionality

--- NCTS.Contracts/Models/DBModels/CallData.cs
+++ NCTS.Contracts/Models/DBModels/CallData.cs
@@ -6,7 +6,7 @@ namespace NCTS.Contracts.Models.DBModels
 {
     public class CallData
     {
-        //public string alarm_name { get; set; }
+        public string alarm_name { get; set; }
         public string call_action { get; set; }
         public string employee_id { get; set; }
         public string application { get; set; }
