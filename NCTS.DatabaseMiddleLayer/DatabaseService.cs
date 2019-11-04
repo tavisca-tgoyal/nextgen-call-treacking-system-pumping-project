2fae888417ebf33fd42f4564feb2acab72e4a291
Ujjwal Bharti <ubharti@tavisca.com>
2019-11-04 14:22:06 +0530

    Added the exception handling of database

--- NCTS.DatabaseMiddleLayer/DatabaseService.cs
+++ NCTS.DatabaseMiddleLayer/DatabaseService.cs
@@ -42,7 +42,13 @@ namespace NCTS.DatabaseMiddleLayer
             var callDataList = callDataProxyService.GetProxyObjects().ToList().ToModel();
             foreach (var callData in callDataList)
             {
-                var sqlString = "call octs.insert_call_data('" + callData.CallAction + "','" + callData.EmployeeCode + "','" + callData.ApplicationName + "','" + callData.Environment + "','" + callData.AlarmName + "','" + callData.TimeStamp + "');";
+                var sqlString = "call octs.insert_call_data('"  + callData.CallAction + "','" 
+                                                                + callData.EmployeeCode + "','" 
+                                                                + callData.ApplicationName + "','" 
+                                                                + callData.Environment + "','" 
+                                                                + callData.AlarmName + "','" 
+                                                                + callData.TimeStamp.ToString("yyyy-MM-dd HH:mm:ss") 
+                                                                + "');";
                 var sqlCommand = new MySqlCommand(sqlString, _sqlConnection);
                 try
                 {
@@ -50,6 +56,54 @@ namespace NCTS.DatabaseMiddleLayer
                 }
                 catch (Exception e)
                 {
+                    var sqlQuery = "call octs.validate_alarm_name('" + callData.AlarmName + "');";
+                    var command = new MySqlCommand(sqlQuery, _sqlConnection);
+                    var result = command.ExecuteReader();
+                    
+                    if(!result.HasRows)
+                    {
+                        if (!result.IsClosed)
+                            result.Close();
+                        sqlQuery = "call octs.insert_alarm('" + callData.AlarmName + "');";
+                        command = new MySqlCommand(sqlQuery, _sqlConnection);
+                        command.ExecuteNonQuery();
+                    }
+                    if (!result.IsClosed)
+                        result.Close();
+                    sqlQuery = "call octs.validate_application_name('" + callData.ApplicationName + "');";
+                    command = new MySqlCommand(sqlQuery, _sqlConnection);
+                    result = command.ExecuteReader();
+                    if (!result.HasRows)
+                    {
+                        if (!result.IsClosed)
+                            result.Close();
+                        sqlQuery = "call octs.insert_application('" + callData.ApplicationName + "');";
+                        command = new MySqlCommand(sqlQuery, _sqlConnection);
+                        command.ExecuteNonQuery();
+                    }
+                    if (!result.IsClosed)
+                        result.Close();
+                    sqlQuery = "call octs.validate_employee_code('" + callData.EmployeeCode + "');";
+                    command = new MySqlCommand(sqlQuery, _sqlConnection);
+                    result = command.ExecuteReader();
+                    if (!result.HasRows)
+                    {
+                        if (!result.IsClosed)
+                            result.Close();
+                        sqlQuery = "call octs.insert_employee('" + new Random().Next(5000,200000) + "','"
+                                                                + callData.EmployeeCode + "','"
+                                                                + "xyz" + "','"
+                                                                + "john" + "','"
+                                                                + "diesel" + "','"
+                                                                + "something@domain.com" + "','"
+                                                                + "100"
+                                                                + "');";
+                        command = new MySqlCommand(sqlQuery, _sqlConnection);
+                        command.ExecuteNonQuery();
+                    }
+                    if (!result.IsClosed)
+                        result.Close();
+                    sqlCommand.ExecuteNonQuery();
                     Log.Information(e.Message.ToString());
                 }
             }
@@ -62,7 +116,11 @@ namespace NCTS.DatabaseMiddleLayer
 
             foreach (var callTree in callTreeList)
             {
-                var sqlString = "call octs.insert_call_tree('" + callTree.ApplicationName + "','" + callTree.Environment + "','" + callTree.Level1Employee + "','" + callTree.Level2Employee + "','" + callTree.Level3Employee + "');";
+                var sqlString = "call octs.insert_call_tree('"  + callTree.ApplicationName + "','" 
+                                                                + callTree.Environment + "','" 
+                                                                + callTree.Level1Employee + "','" 
+                                                                + callTree.Level2Employee + "','" 
+                                                                + callTree.Level3Employee + "');";
                 var sqlCommand = new MySqlCommand(sqlString, _sqlConnection);
                 try
                 {
@@ -70,6 +128,67 @@ namespace NCTS.DatabaseMiddleLayer
                 }
                 catch (Exception e)
                 {
+                    var sqlQuery = "call octs.validate_employee_id('" + callTree.Level1Employee + "');";
+                    var command = new MySqlCommand(sqlQuery, _sqlConnection);
+                    var result = command.ExecuteReader();
+                    if (!result.HasRows)
+                    {
+                        if (!result.IsClosed)
+                            result.Close();
+                        sqlQuery = "call octs.insert_employee('" + callTree.Level1Employee + "','"
+                                                                + 10009 + "','"
+                                                                + "xyz" + "','"
+                                                                + "john" + "','"
+                                                                + "diesel" + "','"
+                                                                + "something@domain.com" + "','"
+                                                                + "100"
+                                                                + "');";
+                        command = new MySqlCommand(sqlQuery, _sqlConnection);
+                        command.ExecuteNonQuery();
+                    }
+                    if (!result.IsClosed)
+                        result.Close();
+                    sqlQuery = "call octs.validate_employee_id('" + callTree.Level2Employee + "');";
+                    command = new MySqlCommand(sqlQuery, _sqlConnection);
+                    result = command.ExecuteReader();
+                    if (!result.HasRows)
+                    {
+                        if (!result.IsClosed)
+                            result.Close();
+                        sqlQuery = "call octs.insert_employee('" + callTree.Level2Employee + "','"
+                                                                + 10010 + "','"
+                                                                + "xyz" + "','"
+                                                                + "john" + "','"
+                                                                + "diesel" + "','"
+                                                                + "something@domain.com" + "','"
+                                                                + "100"
+                                                                + "');";
+                        command = new MySqlCommand(sqlQuery, _sqlConnection);
+                        command.ExecuteNonQuery();
+                    }
+                    if (!result.IsClosed)
+                        result.Close();
+                    sqlQuery = "call octs.validate_employee_id('" + callTree.Level3Employee + "');";
+                    command = new MySqlCommand(sqlQuery, _sqlConnection);
+                    result = command.ExecuteReader();
+                    if (!result.HasRows)
+                    {
+                        if (!result.IsClosed)
+                            result.Close();
+                        sqlQuery = "call octs.insert_employee('" + callTree.Level3Employee + "','"
+                                                                + 10011 + "','"
+                                                                + "xyz" + "','"
+                                                                + "john" + "','"
+                                                                + "diesel" + "','"
+                                                                + "something@domain.com" + "','"
+                                                                + "100"
+                                                                + "');";
+                        command = new MySqlCommand(sqlQuery, _sqlConnection);
+                        command.ExecuteNonQuery();
+                    }
+                    if (!result.IsClosed)
+                        result.Close();
+                    sqlCommand.ExecuteNonQuery();
                     Log.Information(e.Message.ToString());
                 }
             }
@@ -80,15 +199,20 @@ namespace NCTS.DatabaseMiddleLayer
             var employeeHourList = await employeeHour.GetEmployeeHours();
             foreach (var empHour in employeeHourList)
             {
-                var sqlString = "call octs.insert_hours_on_support('" + empHour.EmployeeId + "','" + empHour.Hours + "');";
-                var sqlCommand = new MySqlCommand(sqlString, _sqlConnection);
-                try
+
+                if(empHour.EmployeeId!=0)
                 {
-                    sqlCommand.ExecuteNonQuery();
-                }
-                catch (Exception e)
-                {
-                    Log.Information(e.Message.ToString());
+                    var sqlString = "call octs.insert_hours_on_support('" + empHour.EmployeeId + "','"
+                                                                        + empHour.Hours + "');";
+                    var sqlCommand = new MySqlCommand(sqlString, _sqlConnection);
+                    try
+                    {
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                    catch (Exception e)
+                    {
+                        Log.Information(e.Message.ToString());
+                    }
                 }
             }
         }
@@ -99,7 +223,14 @@ namespace NCTS.DatabaseMiddleLayer
             var employeeList = employeeProxyList.ToModel();
             foreach (var employee in employeeList)
             {
-                var sqlString = "call octs.insert_employee('" + employee.Id + "','" + employee.EmployeeCode + "','" + employee.Squad + "','" + employee.FirstName + "','" + employee.LastName + "','" + employee.Email + "','" + employee.PhoneNumber + "');";
+                var sqlString = "call octs.insert_employee('"   + employee.Id + "','" 
+                                                                + employee.EmployeeCode + "','" 
+                                                                + employee.Squad + "','" 
+                                                                + employee.FirstName + "','" 
+                                                                + employee.LastName + "','" 
+                                                                + employee.Email + "','" 
+                                                                + employee.PhoneNumber 
+                                                                + "');";
                 var sqlCommand = new MySqlCommand(sqlString, _sqlConnection);
                 try
                 {
